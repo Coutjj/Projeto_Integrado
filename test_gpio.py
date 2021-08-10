44bf0f8255d5b17ad603f8b0b8bebeb9f0846d8e
Juan Lima <coutinholima.j@poli.ufrj.br>
2021-08-10 10:01:19 -0300

    Adicionando leitura para os 3 sensores de nivel

--- test_gpio.py
+++ test_gpio.py
@@ -2,7 +2,16 @@ from gpiozero import InputDevice, Button
 from time import sleep
 
 sensor1 = InputDevice(21, pull_up=False)
+sensor2 = InputDevice(20, pull_up=False)
+sensor3 = InputDevice(16, pull_up=False)
 
 while True:
-    print(sensor1.value)
-    sleep(0.2)
+    if sensor1.is_active:
+        print('Sensor 1 acionado')
+    if sensor2.is_active:
+        print('Sensor 2 acionado')
+    if sensor3.is_active:
+        print('Sensor 3 acionado')
+
+
+    sleep(0.5)
