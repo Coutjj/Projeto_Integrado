d81c8c2c3268508df3d8cf1eb196f397f9c815c7
Juan Lima <coutinholima.j@poli.ufrj.br>
2021-10-03 14:05:39 -0300

    Projeto antes do teste

--- telegram_bot_caixa.py
+++ telegram_bot_caixa.py
@@ -1,3 +1,4 @@
+from re import S
 from gpiozero import DigitalInputDevice, SmoothedInputDevice
 from time import sleep
 from statistics import mean
@@ -6,7 +7,11 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 from dotenv import load_dotenv
 from pathlib import Path
 import os
+from threading import Thread
 
+
+water_in_pipe = False
+sentinel_mode = False
 
 dotenvPath = str(Path(__file__).resolve().parent) + '/.env'
 load_dotenv(dotenv_path=dotenvPath)
@@ -26,21 +31,78 @@ def hello(update: Update, context: CallbackContext) -> None:
 
 
 def status(update: Update, context: CallbackContext) -> None:
+    
     message = ''
     if sensor1.is_active:
-        message = "Cisterna em 100%"
+        message = 'Cisterna em 100%'
     elif sensor2.is_active:
-        message = "Cisterna em 50%"
+        message = 'Cisterna em 50%'
     elif sensor3.is_active:
-        message = "Cisterna em 20%"
+        message = 'Cisterna em 20%'
     else:
-        message = "Cisterna vazia"
+        message = 'Cisterna vazia'
 
     if sensor4.is_active:
-        message += "\nBomba ativada e funcionando."
+        message += '\nEntrando agua: Sim.'
+    else:
+        message += '\nEntrando agua: Nao.'
 
-    update.message.reply_text(message)
-    
+    if sentinel_mode == False:
+        message += '\nStatus do monitoramento: desativado.'
+    else:
+        message += '\nStatus do monitoramento: ativado.'
+
+
+    sendMessage(message)
+
+
+def checkPipeStatus():
+
+    global water_in_pipe
+    sleep(5)  
+    while (True):
+        water_in_pipe = sensor4.is_active
+        print('Thread de monitoramento rodando.')
+        
+        global sentinel_mode
+        if sentinel_mode == False:
+            break
+
+        if not water_in_pipe:
+            sendMessage('Desligue a bomba!')
+        elif sensor1.is_active:
+            sendMessage('Cisterna cheia, desligue a bomba!')
+        
+        sleep(5)
+        
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    global sentinel
+    global sentinel_mode
+    sentinel_mode = True
+    sentinel = Thread(target=checkPipeStatus)
+    sendMessage("Processo de monitoramento iniciado.")
+    status(Update, CallbackContext)
+    sentinel.start()
+
+
+def stop(update: Update, context: CallbackContext) -> None:
+    global sentinel_mode
+    global sentinel
+
+    if sentinel_mode == True:
+
+        sendMessage("Processo de monitoramento encerrado.")
+        
+        sentinel_mode = False
+        sentinel.join()
+        print('Thread de monitoramento encerrada')
+    else:
+        sendMessage('O processo de monitoramento nao foi iniciado previamente.')
+
+
+ 
 
 def sendMessage(message):
     id = int(group_id)
@@ -51,11 +113,25 @@ updater = Updater(telegram_token)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('status', status))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('stop', stop))
 
 
-sensor1.when_activated = lambda : sendMessage("Sensor 1 ativado")
-sensor2.when_activated = lambda : sendMessage("Sensor 2 ativado")
-sensor3.when_activated = lambda : sendMessage("Sensor 3 ativado")
+# sensor1.when_activated = lambda : sendMessage("Sensor 1 ativado")
+# sensor2.when_activated = lambda : sendMessage("Sensor 2 ativado")
+# sensor3.when_activated = lambda : sendMessage("Sensor 3 ativado")
+# sensor4.when_activated = lambda : sendMessage("Sensor de presenca de agua ativado")
+# sensor4.when_deactivated = lambda : sendMessage("Sensor de presenca de agua desativado")
+
+sensor1.when_activated = lambda : status(Update, CallbackContext)
+sensor1.when_deactivated = lambda: status(Update, CallbackContext)
+
+sensor2.when_activated = lambda : status(Update, CallbackContext)
+sensor2.when_deactivated = lambda : status(Update, CallbackContext)
+
+sensor3.when_activated = lambda : status(Update, CallbackContext)
+sensor3.when_deactivated = lambda : status(Update, CallbackContext)
+
 sensor4.when_activated = lambda : sendMessage("Sensor de presenca de agua ativado")
 sensor4.when_deactivated = lambda : sendMessage("Sensor de presenca de agua desativado")
 
